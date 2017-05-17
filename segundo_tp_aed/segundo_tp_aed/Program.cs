04cfd01478110e5795614fa4b3ad8b33bef4e804
RODRIGO JUNIOR DE ASSIS <p066391@d02.prodemge.gov.br>
2017-05-17 14:51:05 -0300

    Adicionando o projeto do segundo tp

---
+++ segundo_tp_aed/segundo_tp_aed/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace segundo_tp_aed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamanho = 100000;
+
+            int[] vetor = VetorUtil.gerarAleatorio(tamanho);            
+            var inicio = DateTime.Now;
+            int[] vetorOrdenado = VetorUtil.bubbleSort(vetor);
+            var fim = DateTime.Now;            
+            var tempoGasto = fim - inicio;
+
+            Console.WriteLine(tempoGasto.TotalMilliseconds);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ segundo_tp_aed/segundo_tp_aed/VetorUtil.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace segundo_tp_aed
+{
+    public static class VetorUtil
+    {
+        public static int[] gerarAleatorio(int tamanho)
+        {
+            int[] vetor = new int[tamanho];
+
+            var rand = new Random();
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                vetor[i] = rand.Next(1, tamanho);
+            }
+
+            return vetor;
+        }
+
+        public static int[] bubbleSort(int[] vetor)
+        {
+            bool trocado;
+            int tamanho = vetor.Length;
+            do
+            {
+                trocado = false;
+                tamanho--;
+                for (int i = 0; i < tamanho; i++)
+                {
+                    if (vetor[i] > vetor[i + 1])
+                    {
+                        int aux = vetor[i + 1];
+                        vetor[i + 1] = vetor[i];
+                        vetor[i] = aux;
+                        trocado = true;
+                    }
+                }
+            } while (trocado);
+
+            return vetor;
+        }
+
+        public static void exibirVetor(int[] vetor)
+        {
+            Console.WriteLine("EXIBINDO VETOR:");
+
+            foreach (var numero in vetor)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.WriteLine("---FIM---");
+        }
+    }
+}
